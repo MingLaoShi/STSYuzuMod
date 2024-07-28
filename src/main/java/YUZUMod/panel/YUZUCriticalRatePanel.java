91592880eda6cc9fcd8338e942c9bc148f600e85
马老师 <iammaming@126.com>
2024-07-28 18:46:10 +0800

    总之修bug

--- src/main/java/YUZUMod/panel/YUZUCriticalRatePanel.java
+++ src/main/java/YUZUMod/panel/YUZUCriticalRatePanel.java
@@ -1,14 +1,19 @@
 package YUZUMod.panel;
 
+import YUZUMod.YuzuMod;
+import YUZUMod.helper.ModHelper;
 import YUZUMod.power.YUZUCriticalHitPower;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.ui.panels.AbstractPanel;
 
 import static com.megacrit.cardcrawl.helpers.FontHelper.prepFont;
@@ -16,6 +21,8 @@ import static com.megacrit.cardcrawl.helpers.FontHelper.prepFont;
 public class YUZUCriticalRatePanel extends AbstractPanel {
     public static int OriginMax=10;
     private int MAX;
+    private Texture background= ImageMaster.loadImage(ModHelper.makeImgPath("UI/criticalPanel","background"));
+    private static float ORB_IMG_SCALE = 1.2f * Settings.scale;
 
     public int getAmount() {
         return amount;
@@ -27,30 +34,73 @@ public class YUZUCriticalRatePanel extends AbstractPanel {
     private static final Color FONT_COLOR = new Color(1.0F, 1.0F, 0.86F, 1.0F);
 
     private static BitmapFont expPanelFont = prepFont(FontSize, true);
-
-
+    private static ShapeRenderer shapeRenderer;
+    private float progress=0.0F;
+    private float nextProgress=0.0F;
+    private float duration=0.0F;
     public YUZUCriticalRatePanel(float show_x, float show_y, float hide_x, float hide_y, Texture img, boolean startHidden) {
         super(show_x, show_y, hide_x, hide_y, img, startHidden);
         this.MAX=OriginMax;
         this.amount=0;
         this.modifiedMax=-1;
+        shapeRenderer=new ShapeRenderer();
     }
 
     public void update(){
-        if (this.target_x != this.current_x) {
-            this.current_x = this.target_x;
-        }
-        if (this.target_y != this.current_y) {
-            this.current_y = this.target_y;
+        this.current_x=AbstractDungeon.overlayMenu.energyPanel.current_x;
+        this.current_y=AbstractDungeon.overlayMenu.energyPanel.current_y;
+        if(this.duration>0.0F){
+            this.duration-=Gdx.graphics.getDeltaTime();
+            float step=(this.nextProgress-this.progress)/this.duration*Gdx.graphics.getDeltaTime();
+            if(step>0.0F&&this.progress+step>=this.nextProgress||
+                step<0.0F&&this.progress+step<=this.nextProgress){
+                this.progress=nextProgress;
+            }else{
+                this.progress+=step;
+            }
+        }else{
+            if(this.progress==1.0F){
+                this.duration=0.5F;
+                this.nextProgress=0.0F;
+            }
         }
     }
 
     public void render(SpriteBatch sb){
-        FontHelper.renderFontCentered(sb, expPanelFont, this.amount+"/"+(this.modifiedMax>0?this.modifiedMax:this.MAX), this.current_x + 20.0F * Settings.scale, this.current_y , FONT_COLOR);
+
+        sb.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(YuzuMod.YUZUColor.cpy());
+
+        // 绘制弧形进度条
+        drawArcProgressBar(shapeRenderer, current_x, current_y, 50* Settings.scale, 75*Settings.scale, 270, 360 * progress);
+
+        shapeRenderer.end();
+        sb.begin();
+        sb.setColor(Color.WHITE.cpy());
+//        sb.draw(background,current_x-background.getWidth()/2.0F,current_y-background.getHeight()/2.0F);
+        sb.draw(background,
+                current_x-70.0f, current_y-68.0f ,
+                70.0f, 70.0f,
+                196.0F, 140.0F,
+                ORB_IMG_SCALE, ORB_IMG_SCALE,
+                0.0F,
+                0, 0,
+                196, 140,
+                false, false);
+        this.renderNumbers(sb);
+    }
+
+    private void renderNumbers(SpriteBatch sb) {
+        FontHelper.renderFontCentered(sb,FontHelper.healthInfoFont,Integer.toString(this.amount),this.current_x+100*Settings.scale,this.current_y+30.0F*Settings.scale,Color.WHITE.cpy());
+        int max=getMAX();
+        FontHelper.renderFontCentered(sb,FontHelper.healthInfoFont,Integer.toString(max),this.current_x+100.0F*Settings.scale,this.current_y-10.0F*Settings.scale,Color.WHITE.cpy());
     }
 
     public int increase(int amount){
         this.amount+=amount;
+        calaProgress();
+
         this.generatePower();
         return this.amount;
     }
@@ -75,16 +125,21 @@ public class YUZUCriticalRatePanel extends AbstractPanel {
     public void setMAX(int max){
         this.modifiedMax=max;
         this.modifiedMax=Math.max(1,this.modifiedMax);
+        calaProgress();
         generatePower();
     }
     public void changeMAX(int amount){
         this.MAX+=amount;
         this.MAX=Math.max(1,this.MAX);
+        calaProgress();
+
         generatePower();
     }
 
     public void resetModifierMax(){
         this.modifiedMax=-1;
+        calaProgress();
+
         generatePower();
     }
 
@@ -92,4 +147,36 @@ public class YUZUCriticalRatePanel extends AbstractPanel {
         return modifiedMax>0?modifiedMax:MAX;
     }
 
+    private void drawArcProgressBar(ShapeRenderer renderer, float x, float y, float innerRadius, float outerRadius, float startAngle, float degrees) {
+        int segments = 100;  // 细分数量越高，弧形越平滑
+        float theta = (float) Math.toRadians(degrees) / segments;
+        float cos = (float) Math.cos(theta);
+        float sin = (float) Math.sin(theta);
+
+        float outerX = outerRadius * (float) Math.cos(Math.toRadians(startAngle));
+        float outerY = outerRadius * (float) Math.sin(Math.toRadians(startAngle));
+        float innerX = innerRadius * (float) Math.cos(Math.toRadians(startAngle));
+        float innerY = innerRadius * (float) Math.sin(Math.toRadians(startAngle));
+
+        for (int i = 0; i < segments; i++) {
+            float newOuterX = cos * outerX - sin * outerY;
+            float newOuterY = sin * outerX + cos * outerY;
+            float newInnerX = cos * innerX - sin * innerY;
+            float newInnerY = sin * innerX + cos * innerY;
+
+            renderer.triangle(x + innerX, y + innerY, x + outerX, y + outerY, x + newOuterX, y + newOuterY);
+            renderer.triangle(x + innerX, y + innerY, x + newOuterX, y + newOuterY, x + newInnerX, y + newInnerY);
+
+            outerX = newOuterX;
+            outerY = newOuterY;
+            innerX = newInnerX;
+            innerY = newInnerY;
+        }
+    }
+
+    private void calaProgress(){
+        int tempMax=getMAX();
+        this.nextProgress= (float) this.amount /tempMax;
+        this.duration=0.5F;
+    }
 }
